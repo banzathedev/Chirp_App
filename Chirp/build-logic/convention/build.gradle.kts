9261816098e43e09fe3ad46c5460a3ae0bf7eb2f
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2025-10-15 17:04:38 -0300

    BuildKonfig Convention Plugin

--- Chirp/build-logic/convention/build.gradle.kts
+++ Chirp/build-logic/convention/build.gradle.kts
@@ -12,6 +12,8 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.compose.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
+    implementation(libs.buildkonfig.gradlePlugin)
+    implementation(libs.buildkonfig.compiler)
 }
 
 java {
@@ -57,6 +59,10 @@ gradlePlugin {
         register("cmpFeature") {
             id = "com.grpitsolutions.convention.cmp.feature"
             implementationClass = "CmpFeatureConventionPlugin"
+        }
+        register("buildKonfig") {
+            id = "com.grpitsolutions.convention.buildkonfig"
+            implementationClass = "BuildKonfigConventionPlugin"
         }
     }
 }
--- Chirp/feature/chat/data/src/commonMain/kotlin/com/grpitsolutions/chat/data/Platform.kt
+++ Chirp/feature/chat/data/src/commonMain/kotlin/com/grpitsolutions/chat/data/Platform.kt
@@ -1,3 +1,5 @@
 package com.grpitsolutions.chat.data
 
+import com.grpitsolutions.feature.chat.data.BuildKonfig
+
 expect fun platform(): String
