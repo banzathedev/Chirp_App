2ab6c1266c83e607b9c1bc3f7234c74481ed2bb1
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2025-10-15 15:08:06 -0300

    Android Application Convention Plugin

--- Chirp/composeApp/build.gradle.kts
+++ Chirp/composeApp/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.convention.android.application)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.compose.hot.reload)
@@ -25,8 +25,6 @@ kotlin {
             isStatic = true
         }
     }
-
-    jvm()
 
     sourceSets {
         androidMain.dependencies {
@@ -64,45 +62,6 @@ kotlin {
     }
 }
 
-android {
-    namespace = "com.grpitsolutions.chirp"
-    compileSdk = libs.versions.projectCompileSdkVersion.get().toInt()
-
-    defaultConfig {
-        applicationId = "com.grpitsolutions.chirp"
-        minSdk = libs.versions.projectMinSdkVersion.get().toInt()
-        targetSdk = libs.versions.projectTargetSdkVersion.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
 dependencies {
     debugImplementation(compose.uiTooling)
-}
-
-compose.desktop {
-    application {
-        mainClass = "com.grpitsolutions.chirp.MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.grpitsolutions.chirp"
-            packageVersion = "1.0.0"
-        }
-    }
 }
