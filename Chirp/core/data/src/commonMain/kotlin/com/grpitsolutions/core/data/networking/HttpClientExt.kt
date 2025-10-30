b9bcc8f44280f218d0dede2bea8acc6b158b18f4
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2025-10-30 13:42:03 -0300

    HttpClient Network Call Utility

--- Chirp/core/data/src/commonMain/kotlin/com/grpitsolutions/core/data/networking/HttpClientExt.kt
+++ Chirp/core/data/src/commonMain/kotlin/com/grpitsolutions/core/data/networking/HttpClientExt.kt
@@ -2,9 +2,20 @@ package com.grpitsolutions.core.data.networking
 
 import com.grpitsolutions.core.domain.util.DataError
 import com.grpitsolutions.core.domain.util.Result
+import io.ktor.client.HttpClient
 import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+
 
 expect suspend fun <T> platformSafeCall(
     execute: suspend () -> HttpResponse,
@@ -42,6 +53,75 @@ suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<
         else -> Result.Failure(DataError.Remote.UNKNOWN)
     }
 }
+
+suspend inline fun <reified Request, reified Response: Any> HttpClient.post(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        post {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Response: Any> HttpClient.get(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        get {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Response: Any> HttpClient.delete(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        delete {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Request, reified Response: Any> HttpClient.put(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        put {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
+
 
 fun constructRoute(route: String): String {
     return when {
