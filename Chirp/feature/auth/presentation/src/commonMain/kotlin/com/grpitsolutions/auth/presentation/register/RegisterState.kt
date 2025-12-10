ce7506c3b8289593c9948b88e0cd3f3d26a00f92
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2025-12-10 13:39:57 -0300

    staring register screen
    add an cool file template to make boilerplate screens for compose with MVI

---
+++ Chirp/feature/auth/presentation/src/commonMain/kotlin/com/grpitsolutions/auth/presentation/register/RegisterState.kt
@@ -0,0 +1,18 @@
+package com.grpitsolutions.auth.presentation.register
+
+import androidx.compose.foundation.text.input.TextFieldState
+import com.grpitsolutions.core.presentation.util.UiText
+
+data class RegisterState(
+    val emailTextState: TextFieldState = TextFieldState(),
+    val isEmailValid: Boolean = false,
+    val emailError: UiText? = null,
+    val passwordTextState: TextFieldState = TextFieldState(),
+    val isPasswordValid: Boolean = false,
+    val passwordError: UiText? = null,
+    val usernameTextState: TextFieldState = TextFieldState(),
+    val isUsernameValid: Boolean = false,
+    val registrationError: UiText? = null,
+    val isRegistering: Boolean = false,
+    val canRegister: Boolean = false
+)
