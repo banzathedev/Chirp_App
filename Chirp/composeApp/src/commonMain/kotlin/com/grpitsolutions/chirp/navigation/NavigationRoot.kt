f32052871634b961848185a048a8311e1bc6be2d
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2026-01-08 16:12:23 -0300

    navigation started
    start navigation.
    
    here we willl be using compose navigation, since nav3 is not yet available for kmp.

---
+++ Chirp/composeApp/src/commonMain/kotlin/com/grpitsolutions/chirp/navigation/NavigationRoot.kt
@@ -0,0 +1,12 @@
+package com.grpitsolutions.chirp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.Navigation
+
+@Composable
+fun NavigationRoot(
+    modifier: Modifier
+){
+    
+}
---
+++ Chirp/feature/auth/presentation/src/commonMain/kotlin/com/grpitsolutions/auth/presentation/navigation/AuthGrap.kt
@@ -0,0 +1,28 @@
+package com.grpitsolutions.auth.presentation.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.grpitsolutions.auth.presentation.register.RegisterRoot
+import com.grpitsolutions.auth.presentation.register_success.RegisterSuccessRoot
+
+fun NavGraphBuilder.authGraph(
+    navController: NavController,
+    onLoginSuccess: () -> Unit
+) {
+    navigation<AuthGraphRoutes.Graph>(
+        startDestination = AuthGraphRoutes.Register
+    ) {
+        composable<AuthGraphRoutes.Register> {
+            RegisterRoot(
+                onRegisterSuccess = {
+                navController.navigate(AuthGraphRoutes.RegisterSuccess(it))
+                }
+            )
+        }
+        composable<AuthGraphRoutes.RegisterSuccess> {
+            RegisterSuccessRoot()
+        }
+    }
+}
