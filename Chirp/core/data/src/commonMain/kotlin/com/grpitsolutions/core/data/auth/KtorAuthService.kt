492d5efbab1aa279a3b1a44bf31b575ccff5b541
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2026-01-12 18:09:08 -0300

    Linked email verification logic

--- Chirp/core/data/src/commonMain/kotlin/com/grpitsolutions/core/data/auth/KtorAuthService.kt
+++ Chirp/core/data/src/commonMain/kotlin/com/grpitsolutions/core/data/auth/KtorAuthService.kt
@@ -2,6 +2,7 @@ package com.grpitsolutions.core.data.auth
 
 import com.grpitsolutions.core.data.dto.request.EmailRequest
 import com.grpitsolutions.core.data.dto.request.RegisterRequest
+import com.grpitsolutions.core.data.networking.get
 import com.grpitsolutions.core.data.networking.post
 import com.grpitsolutions.core.domain.auth.AuthService
 import com.grpitsolutions.core.domain.util.DataError
@@ -30,6 +31,13 @@ class KtorAuthService(
         return httpClient.post(
             route = "/auth/resend-verification",
             body = EmailRequest(email)
+        )
+    }
+
+    override suspend fun verifyEmail(token: String): EmptyResult<DataError.Remote> {
+        return httpClient.get(
+            route = "/auth/verify",
+            queryParams = mapOf("token" to token)
         )
     }
 }
--- Chirp/feature/auth/presentation/src/commonMain/kotlin/com/grpitsolutions/auth/presentation/email_verification/EmailVerification.kt
+++ Chirp/feature/auth/presentation/src/commonMain/kotlin/com/grpitsolutions/auth/presentation/email_verification/EmailVerification.kt
@@ -35,10 +35,11 @@ import com.grpitsolutions.core.designsystem.components.layouts.ChirpSimpleResult
 import com.grpitsolutions.core.designsystem.theme.ChirpTheme
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun EmailVerificationRoot(
-    viewModel: EmailVerificationViewModel = viewModel()
+    viewModel: EmailVerificationViewModel = koinViewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
