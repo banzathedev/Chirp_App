1da294afe1888b4658181d93bc9a80181afa1062
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2026-01-15 10:06:05 -0300

    Password reset screen layout done
    still missing logic, next commit may be the logic finished.

--- Chirp/feature/auth/presentation/src/commonMain/kotlin/com/grpitsolutions/auth/presentation/di/AuthPresetationModule.kt
+++ Chirp/feature/auth/presentation/src/commonMain/kotlin/com/grpitsolutions/auth/presentation/di/AuthPresetationModule.kt
@@ -5,6 +5,7 @@ import com.grpitsolutions.auth.presentation.forgot_password.ForgotPasswordViewMo
 import com.grpitsolutions.auth.presentation.login.LoginViewModel
 import com.grpitsolutions.auth.presentation.register.RegisterViewModel
 import com.grpitsolutions.auth.presentation.register_success.RegisterSuccessViewModel
+import com.grpitsolutions.auth.presentation.reset_password.ResetPasswordViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -14,4 +15,5 @@ val authPresentationModule = module {
     viewModelOf(::EmailVerificationViewModel)
     viewModelOf(::LoginViewModel)
     viewModelOf(::ForgotPasswordViewModel)
+    viewModelOf(::ResetPasswordViewModel)
 }
