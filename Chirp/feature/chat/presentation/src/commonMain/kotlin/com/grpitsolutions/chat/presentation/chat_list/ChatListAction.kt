13e25f34285b9de1af6644cc31e2830ffd1af37b
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2026-01-30 10:59:28 -0300

    chat list screen scaffold
    scaffold created, this is all the time i have for today, so implementation will continue later day.

--- Chirp/feature/chat/presentation/src/commonMain/kotlin/com/grpitsolutions/chat/presentation/chat_list/ChatListAction.kt
+++ Chirp/feature/chat/presentation/src/commonMain/kotlin/com/grpitsolutions/chat/presentation/chat_list/ChatListAction.kt
@@ -6,6 +6,7 @@ sealed interface ChatListAction {
     data object OnLogoutClick: ChatListAction
     data object OnConfirmLogout: ChatListAction
     data object OnDismissLogoutDialog: ChatListAction
+    data object OnCreateChatClick: ChatListAction
     data class OnChatClick(
         val chatId: String
     ): ChatListAction
--- Chirp/feature/chat/presentation/src/commonMain/kotlin/com/grpitsolutions/chat/presentation/chat_list/ChatListScreen.kt
+++ Chirp/feature/chat/presentation/src/commonMain/kotlin/com/grpitsolutions/chat/presentation/chat_list/ChatListScreen.kt
@@ -1,9 +1,26 @@
 package com.grpitsolutions.chat.presentation.chat_list
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import chirp.feature.chat.presentation.generated.resources.Res
+import chirp.feature.chat.presentation.generated.resources.create_chat
+import com.grpitsolutions.core.designsystem.components.buttons.ChirpFloatingActionButton
 import com.grpitsolutions.core.designsystem.theme.ChirpTheme
+import com.grpitsolutions.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -12,10 +29,13 @@ fun ChatListRoot(
     viewModel: ChatListViewModel = koinViewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val snackBarHostState = remember { SnackbarHostState() }
+
 
     ChatListScreen(
         state = state,
-        onAction = viewModel::onAction
+        onAction = viewModel::onAction,
+        snackBarHostState = snackBarHostState
     )
 }
 
@@ -23,8 +43,32 @@ fun ChatListRoot(
 fun ChatListScreen(
     state: ChatListState,
     onAction: (ChatListAction) -> Unit,
+    snackBarHostState: SnackbarHostState
 ) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.extended.surfaceLower,
+        contentWindowInsets = WindowInsets.safeDrawing,
+        snackbarHost = { SnackbarHost(snackBarHostState) },
+        floatingActionButton = {
+            ChirpFloatingActionButton(
+                onClick = {
+                    onAction(ChatListAction.OnCreateChatClick)
+                },
+                content = {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = stringResource(Res.string.create_chat)
+                    )
+                }
+            )
+        }
 
+    ) { innerPadding ->
+
+
+    }
 }
 
 @Preview
@@ -33,7 +77,8 @@ private fun Preview() {
     ChirpTheme {
         ChatListScreen(
             state = ChatListState(),
-            onAction = {}
+            onAction = {},
+            snackBarHostState = remember { SnackbarHostState() }
         )
     }
 }
